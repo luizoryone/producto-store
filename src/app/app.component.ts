f90afa38cdc7da6114269a1406721344f4d97d1d
luizoryone <luizoryone@gmail.com>
2024-05-24 12:33:48 -0300

    feat: adiciona Mattoobar ao compomente heade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatToolbarModule } from "@angular/material/toolbar/";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,MatToolbarModule],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
