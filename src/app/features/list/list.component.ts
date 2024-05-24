01c95f69d37e8b47a6aee8a1bcebf0fc8904bf5d
luizoryone <luizoryone@gmail.com>
2024-05-24 18:22:30 -0300

    feat: adicionado componente de listagem

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+
+}
